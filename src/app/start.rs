2122240b137007ace19caade70414d7d11160b47
Martin Ždila <m.zdila@gmail.com>
2026-03-20 09:34:12 +0100

    removed sleeping

--- src/app/start.rs
+++ src/app/start.rs
@@ -168,10 +168,6 @@ pub(crate) fn start() {
     println!("Stopping render worker pool.");
     render_worker_pool.shutdown();
     println!("Render worker pool stopped.");
-
-    println!("Sleeping for 30 seconds.");
-    sleep(Duration::from_secs(30));
-    println!("Done.");
 }
 
 fn build_tile_variants(cli: &Cli) -> Result<Vec<TileVariantOptions>, String> {
